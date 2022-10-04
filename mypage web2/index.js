098ed9134e4d9cc0eff573007fe180718ae441c6
ssutsen <babe920604@gmail.com>
2022-10-04 16:45:30 +0800

    w4

--- mypage web2/index.js
+++ mypage web2/index.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+var mysql=require("mysql")
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    password:"babe96857411",
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("connect!");
+});
