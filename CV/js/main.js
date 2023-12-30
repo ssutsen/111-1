2ac6fe4a1ca0b3bffdb597c0fc06d143e015c3ce
ssutsen <babe920604@gmail.com>
2023-12-30 18:39:37 +0800

    CV done1

--- CV/js/main.js
+++ CV/js/main.js
@@ -84,7 +84,7 @@ if($().circleProgress){
 
 	//Set progress circle 1
 	$("#progress1").circleProgress({
-		value: 0.75,
+		value: 1,
 		size: 175,
 		thickness: 2,
 		fill: "#40424a",
@@ -92,7 +92,7 @@ if($().circleProgress){
 	});
 	//Set progress circle 2
 	$("#progress2").circleProgress({
-		value: 0.83,
+		value: 0.7,
 		size: 175,
 		thickness: 2,
 		fill: "#40424a",
@@ -101,19 +101,19 @@ if($().circleProgress){
 
 	//Set progress circle white
 	$("#progress3").circleProgress({
-		value: 0.75,
+		value: 0.7,
 		size: 175,
 		thickness: 2,
-		fill: "#ffffff",
+		fill: "#40424a",
 		emptyFill: "rgba(0, 0, 0, 0)"
 	});
 
 	//Set progress circle white
 	$("#progress4").circleProgress({
-		value: 0.83,
+		value: 0.4,
 		size: 175,
 		thickness: 2,
-		fill: "#ffffff",
+		fill: "#40424a",
 		emptyFill: "rgba(0, 0, 0, 0)"
 	});
 
